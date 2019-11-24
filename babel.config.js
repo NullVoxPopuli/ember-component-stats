d4ad2e313ce4e41af059c821cce2f0731095ee3b
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-11-24 17:17:40 -0500

    bring back node preset

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = {
-  presets: [require('@babel/preset-typescript')],
+  presets: [
+    require('@babel/preset-typescript'),
+    [require('babel-preset-latest-node'), { target: 'current' }],
+  ],
 };
